47cc87d413734973777e96bf00400993c9e463e9
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2023-02-17 11:21:55 +0900

    ADD: start_auto.sh

---
+++ start_auto.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+VENVDIR=$HOME/env2-robottank
+BINDIR=$VENVDIR/bin
+TOPDIR=$VENVDIR/RobotTank1
+WORKDIR=$TOPDIR/DistanceVL53L0X
+
+cd $WORKDIR
+while true; do
+    . $BINDIR/activate
+    echo "VIRTUAL_ENV=$VIRTUAL_ENV"
+
+    python3 -m distancevl53l0x robottankauto 1
+
+    deactivate
+
+    echo -----
+    sleep 2
+done
